3d7e414acfc693b118836b83514ad965a512eb44
steve zhang <longhairknight@hotmail.com>
2015-11-01 20:02:19 +0800

    add consumer thread

--- storage/storlistener.cpp
+++ storage/storlistener.cpp
@@ -1,15 +1,28 @@
 #include "./storlistener.h"
 
-storage_listener::storage_listener(command_pusher pusher, storage_command_q * q, std::string ip, int port){
+storage_listener::storage_listener(command_pusher pusher, command_puller puller, storage_command_q * q, std::string ip, int port){
 
 	m_pusher = pusher;
+	m_puller = puller;
     m_q = q;
 	m_ip = ip;
 	m_port = port;
 	return;
 }
 
+void storage_listener::consumer(command_puller puller, storage_command_q * q){
+	std::unique_lock<std::mutex> lck(mtx);
+	for(;;){
+		cv.wait(lck);
+		while(q->QSize()>0) {
+			storage_command * ret = puller(q);
+			delete ret;
+		}
+		//sleep(1000);
+	}
+}
 void storage_listener::listener(command_pusher pusher, storage_command_q * q, std::string ip, int port){
+	std::unique_lock<std::mutex> lck(mtx);
 	
 	int udpsock = networkhelper::CreateUDPv4(ip, port);
 	
@@ -37,6 +50,7 @@ void storage_listener::listener(command_pusher pusher, storage_command_q * q, st
 		  continue;
 		cout<<cmd->getCommand()<<"\n";
         pusher(q, cmd);
+		cv.notify_one();
 		cout<<"QueueSize:"<<q->QSize()<<"\n";
 		cmd = new storage_command(1024*4);
 
@@ -50,6 +64,7 @@ bool storage_listener::start(){
 	
 	try {
 		m_listener = new thread (listener, m_pusher, m_q, m_ip, m_port);
+		m_consumer = new thread (consumer, m_puller, m_q);
 	}catch(...){
 		cerr<<"fail to create thread to start listener\n";
 		return false;
--- storage/storlistener.h
+++ storage/storlistener.h
@@ -6,22 +6,30 @@
 #include <iostream>
 #include <string>
 #include "../util/network.h"
+#include <mutex>
+#include <condition_variable>
 
 using namespace std;
 
+mutex mtx;
+condition_variable cv;
+
 class storage_listener {
 	public:
-		storage_listener(command_pusher pusher, storage_command_q * q, string ip, int port);
+		storage_listener(command_pusher pusher, command_puller puller, storage_command_q * q, string ip, int port);
 		bool start();
 		bool stop();
 		thread * getThread();
 
 	private:
 		command_pusher m_pusher;
+		command_puller m_puller;
 		string m_ip;
         storage_command_q * m_q;
 		int m_port;
 		thread* m_listener;
+		thread* m_consumer;
 		static void listener(command_pusher pusher, storage_command_q* q, string ip, int port);
+		static void consumer(command_puller puller, storage_command_q* q);
 };
 #endif
